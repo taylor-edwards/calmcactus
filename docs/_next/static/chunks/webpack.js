a33c394979bd295ad54e0bf30593941534ea5334
Taylor Edwards <me@tayloredwards.net>
2021-04-22 16:11:52 -0700

    Deploy fix for misaligned logo on mobile

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c87e33bdde7f8fbb42d4";
+/******/ 	var hotCurrentHash = "071190d6f8577fa35071";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
